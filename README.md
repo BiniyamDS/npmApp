79ed6424053371a0f37e56d697d3de54ce641a1a
BiniyamDS <biniyam.yr@gmail.com>
2024-02-21 21:59:49 +0300

    added live reload and timezone

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Simple web app to utilize npm modules
 Simple app to diplay current time and timezone
-Uses `dayjs`
+# Dependancies
+Uses `dayjs` and `micromodal`
+# New features added
++ Change time zones
++ Live reload
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const MicroModal = require('micromodal')
 const dayjs = require('dayjs')
 const utc = require('dayjs/plugin/utc')
 const timezone = require('dayjs/plugin/timezone') // dependent on utc plugin
@@ -6,15 +7,47 @@ dayjs.extend(utc)
 dayjs.extend(timezone)
 
 // Variables to hold the elements to be changed
-const timeZone = document.getElementById('timezone')
+const timeZoneElem = document.getElementById('timezone')
 const CurTime = document.getElementById('time')
 const curDate = document.getElementById('date')
+const applyBt = document.getElementById('apply-bt')
+const selectElem = document.getElementById('timeZLi')
 
-const now = dayjs()
-console.log(now.format('HH:mm:ss'))
-console.log(now.format('dddd, D MMMM, YYYY'))
-const timeZ = dayjs.tz.guess().replace('/', ' / ').replace('_', ' ')
-console.log(timeZ)
-timeZone.textContent = timeZ
-CurTime.textContent = now.format('hh:mm:ss')
-curDate.textContent = now.format('dddd, D MMMM, YYYY')
+// declare a timezone in user's current location
+let timeZObj = dayjs.tz.guess()
+
+// initialize the element valaues
+popScreen();
+
+// function to populate the date and time zone screen elements
+function popScreen(){
+    const now = dayjs().tz(timeZObj)
+    timeZoneElem.textContent = timeZObj.replace('/', ' / ').replace('_', ' ')
+    CurTime.textContent = now.format('hh:mm:ss')
+    curDate.textContent = now.format('dddd, D MMMM, YYYY')
+}
+
+
+// generate dropdown list of time zones
+let SupTZArr = Intl.supportedValuesOf("timeZone");
+for (const timz of SupTZArr){
+    // Create a new option
+    let newOption = new Option(timz, timz);
+
+    // Add the new option to the select element
+    selectElem.add(newOption, undefined);
+
+}
+
+// modal
+MicroModal.default.init()
+applyBt.addEventListener('click', changeTimeZone);
+
+// function to change the time zone to selected one
+function changeTimeZone(){
+    const selVal = selectElem.value
+    timeZObj = selVal
+}
+
+// reload the time every second
+setInterval(popScreen,  1000);
